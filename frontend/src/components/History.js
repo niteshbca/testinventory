7a481416965a265c7e6e8f71dc987eb9de9da8be
Nitesh Kumar Singh <138958926+niteshbca@users.noreply.github.com>
2025-05-26 16:31:35 +0530

    Update History.js

--- frontend/src/components/History.js
+++ frontend/src/components/History.js
@@ -7,7 +7,7 @@ const History = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/api/data');
+        const response = await axios.get('http://13.235.75.71:5000/api/data');
         setData(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
