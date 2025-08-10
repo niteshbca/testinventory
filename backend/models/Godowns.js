dc50dbf360f44414a33f4f485c698e628fff7cb5
Nitesh Kumar Singh <138958926+niteshbca@users.noreply.github.com>
2025-08-11 00:10:56 +0530

    Delete backend directory

--- backend/models/Godowns.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const godownSchema = new mongoose.Schema({
-  name: { type: String, required: true }, // Godown name
-  address: { type: String, required: true }, // Godown address
-});
-
-module.exports = mongoose.model('Godown', godownSchema); // Model name remains singular
