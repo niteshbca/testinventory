cb2dc648e00e5759001cb836bef070f0cb528beb
Nitesh Kumar Singh <138958926+niteshbca@users.noreply.github.com>
2025-10-01 00:06:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 const Godown = require('./models/Godowns');
 const GodownInventory = require('./models/GodownInventory');
